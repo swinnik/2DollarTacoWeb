0d23c35f0945be694554bd9ef1a25d83283f6de4
Sean Winnik <winnik.sean@gmail.com>
2023-08-26 15:35:17 -0700

    google maps api

--- client/src/components/NewTacoComps/MapContainer.jsx
+++ client/src/components/NewTacoComps/MapContainer.jsx
@@ -9,7 +9,7 @@ export default function MapContainer({ tacoDetails }) {
   // }
 
   return (
-    <LoadScript googleMapsApiKey="AIzaSyB0cbZB8XGRZwBK9g2NzJPywdq4tqG-s8c">
+    <LoadScript googleMapsApiKey={process.env.googleMapsApiKey}>
       <GoogleMap center={{ lat: latitude, lng: longitude }} zoom={15}>
         {/* Add map markers, overlays, or other components */}
       </GoogleMap>
